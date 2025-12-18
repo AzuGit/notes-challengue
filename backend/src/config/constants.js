0ee10997f8853e7e269477ccc29c4737b681d0bd
Angel Azuaje <115132408+AzuGit@users.noreply.github.com>
2025-12-18 19:26:50 -0400

    env changes

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -7,9 +7,8 @@ dotenv.config({
 export const DB_NAME = "ensolver_test_db";
 
 // JWT Constants should be set in environment variables for security
-export const connectionString = `mongodb+srv://usertest:usertest@cluster0.hfok7hc.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-export const JWT_SECRET =
-  process.env.JWT_SECRET || "d75206a9b4d8e4263fb11c44bb5ffc9b";
+export const connectionString = process.env.CONNECTION_STRING;
+export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 8000;
